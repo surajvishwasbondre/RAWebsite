0e002f974bf2022c3f1aed76670816bd855e3e65
surajvishwasbondre <114866975+surajvishwasbondre@users.noreply.github.com>
2025-04-08 18:04:00 +0530

    Update TestBase.java

--- src/test/java/com/tests/TestBase.java
+++ src/test/java/com/tests/TestBase.java
@@ -40,7 +40,7 @@ public class TestBase {
 	//proprties file reading
 		try{
 		prop=new Properties();//1
-		FileInputStream ip=new FileInputStream("D:\\Eclipse Workplace\\RAWebsite\\src\\test\\resources\\config.properties");//2
+		FileInputStream ip=new FileInputStream("src\\test\\resources\\config.properties");//2
 		prop.load(ip);//3
 	}catch(FileNotFoundException e)
 	{
@@ -51,7 +51,7 @@ public class TestBase {
 	}
 	//log file setup
 	logger=Logger.getLogger(TestBase.class);  //1
-	PropertyConfigurator.configure("D:\\Eclipse Workplace\\RAWebsite\\src\\test\\resources\\log4j.properties");//2
+	PropertyConfigurator.configure("src\\test\\resources\\log4j.properties");//2
 	//it is for configuring the log4j setup to our app
 	}
 	
